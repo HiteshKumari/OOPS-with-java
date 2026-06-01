82b672ecde384cdf5df0bcbbd4b35a1575a61c41
HiteshKumari <24bcs10099@cuchd.in>
2026-06-01 08:06:13 +0530

    Add ArrayListDemo class to demonstrate ArrayList usage

---
+++ week1/ArrayListDemo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Spring");
+        list.add("Hibernate");
+
+        System.out.println(list);
+    }
+}
