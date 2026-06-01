7631755c8b2b2f859ee4d21e91f852ac7b905007
HiteshKumari <24bcs10099@cuchd.in>
2026-06-01 07:53:46 +0530

    Add VariablesDemo class with basic variable types

---
+++ week1/VariablesDemo.java
@@ -0,0 +1,14 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Java Fun: " + isJavaFun);
+    }
+}
