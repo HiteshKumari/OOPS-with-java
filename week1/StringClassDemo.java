bbd64612bb78cea28d611110b356cf42b64f4fbf
HiteshKumari <24bcs10099@cuchd.in>
2026-06-01 08:03:43 +0530

    Create StringClassDemo.java

---
+++ week1/StringClassDemo.java
@@ -0,0 +1,10 @@
+public class StringClassDemo {
+    public static void main(String[] args) {
+
+        String name = "Java Internship";
+
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.substring(0, 4));
+    }
+}
