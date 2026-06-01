783f16e2193ea4d3f391d365ce95d46e46fe20d3
HiteshKumari <24bcs10099@cuchd.in>
2026-06-01 08:00:15 +0530

    Implement polymorphism with Animal and Dog classes

---
+++ week1/PolymorphismDemo.java
@@ -0,0 +1,19 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal Sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog Barks");
+    }
+}
+
+public class PolymorphismDemo {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.sound();
+    }
+}
