2824734a1598c62f6b5e1c20fe3bcdd0ac0741ea
HiteshKumari <24bcs10099@cuchd.in>
2026-06-01 08:00:40 +0530

    Add HasARelationshipDemo with Engine and Car classes

---
+++ week1/HasARelationshipDemo.java
@@ -0,0 +1,21 @@
+class Engine {
+    void start() {
+        System.out.println("Engine Started");
+    }
+}
+
+class Car {
+    Engine engine = new Engine();
+
+    void drive() {
+        engine.start();
+        System.out.println("Car Running");
+    }
+}
+
+public class HasARelationshipDemo {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.drive();
+    }
+}
