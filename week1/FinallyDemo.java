4a8dbf5eae2fdf096727235dae4be18041432ec4
HiteshKumari <24bcs10099@cuchd.in>
2026-06-01 08:04:10 +0530

    Add FinallyDemo class to demonstrate finally block

---
+++ week1/FinallyDemo.java
@@ -0,0 +1,12 @@
+public class FinallyDemo {
+    public static void main(String[] args) {
+
+        try {
+            int result = 10 / 0;
+        } catch (Exception e) {
+            System.out.println("Exception Occurred");
+        } finally {
+            System.out.println("Finally Block Executed");
+        }
+    }
+}
